a667564a153e3f8acaca3e7e8aa661c915993368
Sivakeerthivasan <kristenkeerthi123@gmail.com>
2024-02-28 15:21:31 +0530

    Member Login page cmplete

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:techtrack/Pages/DashBoard.dart';
+import 'package:techtrack/Pages/MembersLoginPage.dart';
 import 'Pages/IntroPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
   _initializeFirebase();
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: MemberLoginPage(),
     );
   }
 }
