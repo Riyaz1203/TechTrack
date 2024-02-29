291b484b5131df0ab4a05b9373e21bf86d923ff5
Sivakeerthivasan <kristenkeerthi123@gmail.com>
2024-02-29 16:25:11 +0530

    Raise Issue Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:techtrack/Pages/AccessPage.dart';
+import 'package:techtrack/Pages/AssignPage.dart';
 import 'package:techtrack/Pages/DashBoard.dart';
+import 'package:techtrack/Pages/Homepage.dart';
+import 'package:techtrack/Pages/MainMenu.dart';
 import 'package:techtrack/Pages/MembersLoginPage.dart';
+import 'package:techtrack/Pages/RaiseIssuePage.dart';
 import 'Pages/IntroPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -8,7 +13,6 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
   runApp(const MyApp());
   _initializeFirebase();
@@ -25,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: MemberLoginPage(),
+      home: const RaiseIssue(),
     );
   }
 }
