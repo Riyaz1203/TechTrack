6bb0c03d83c3d15ef58fc17ff88ffe531de76072
godaminator02 <guruprasathjay@gmail.com>
2024-03-01 14:09:22 +0530

    borrowlistpage UI created

--- lib/Pages/BorrowListPage.dart
+++ lib/Pages/BorrowListPage.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:techtrack/Pages/MainMenu.dart';
 
 class BorrowerListPage extends StatelessWidget {
-  const BorrowerListPage({super.key});
+  const BorrowerListPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Users"),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const MainMenu()));
+            },
+            icon: const Icon(Icons.menu),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: screenHeight, // You might want to adjust this
+              width: screenWidth,
+              color: Colors.grey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/keyboard.png"),
+                  SizedBox(height: 40),
+                  Text(
+                    "ID Number         : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Name                 : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Department      : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Type                  : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "category           : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Model No          : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Serial No           : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Brand                : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Status               : Firebase",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
+                  ),
+                  // Add other user details similarly
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:techtrack/Pages/BorrowerPage.dart';
+import 'package:techtrack/Pages/BorrowListPage.dart';
 
 import 'firebase_options.dart';
 
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: BorrowerPage(),
+      home: BorrowerListPage(),
     );
   }
 }
